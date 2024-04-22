3ac1139580501ddc12977d4eec8518ee53f400ff
hotelitaliapalace <167652064+hotelitaliapalace@users.noreply.github.com>
2024-04-22 05:10:23 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-drone-panorama-lignano-500-m",
+      "name": "drone panorama lignano 500 m",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.91180681141533,
+        "pitch": 1.456131160362096,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.16177892654814485,
+          "pitch": 1.1799911124096614,
+          "title": "Hotel Italia Palace",
+          "text": "Lignano Sabbiadoro"
+        },
+        {
+          "yaw": -0.301216780625321,
+          "pitch": 1.2704151343715147,
+          "title": "Hotel Italia Palace",
+          "text": "DEPENDANCE ADULTS ONLY"
+        }
+      ]
+    },
+    {
+      "id": "1-dji_0100",
+      "name": "DJI_0100",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11825283125158137,
+        "pitch": 0.5237472228050013,
+        "fov": 0.7183742073784295
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_0101",
+      "name": "DJI_0101",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.42513763241086977,
+        "pitch": 1.4850260677936085,
+        "fov": 0.6986130231011655
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.40833091561263046,
+          "pitch": 1.501832784591854,
+          "title": "HOTEL ITALIA PALACE",
+          "text": "Lignano Sabbiadoro"
+        }
+      ]
+    }
+  ],
+  "name": "Hotel Italia Palace",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
